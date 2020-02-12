b26f94dc271726238d6d5ffeef83afce08f3efdf
codewizard <mike@bokkagroup.com>
2020-02-11 19:04:48 -0700

    Fixing

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'underDEV\\Utils\\' => array($vendorDir . '/underdev/settings/src'),
+    'Composer\\Installers\\' => array($vendorDir . '/composer/installers/src/Composer/Installers'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,16 +7,16 @@ namespace Composer\Autoload;
 class ComposerStaticInitdde607f96faa1b43935af1e9c51b647a
 {
     public static $prefixLengthsPsr4 = array (
-        'u' => 
+        'C' => 
         array (
-            'underDEV\\Utils\\' => 15,
+            'Composer\\Installers\\' => 20,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'underDEV\\Utils\\' => 
+        'Composer\\Installers\\' => 
         array (
-            0 => __DIR__ . '/..' . '/underdev/settings/src',
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
         ),
     );
 
@@ -51,21 +51,6 @@ class ComposerStaticInitdde607f96faa1b43935af1e9c51b647a
         'SPC_Block_Content' => __DIR__ . '/../..' . '/skins/content-block.php',
         'SPC_Block_Footer' => __DIR__ . '/../..' . '/skins/footer-block.php',
         'WP_Admin_Dismissible_Notice' => __DIR__ . '/..' . '/max-kk/wp-notice-lib/class-admin-dismissible-notice.php',
-        'WP_Skin_Base_Abstract' => __DIR__ . '/..' . '/max-kk/wp-skins-customizer/includes/class-wp-skin-base-abstract.php',
-        'WP_Skins_Customizer' => __DIR__ . '/..' . '/max-kk/wp-skins-customizer/includes/class-wp-skins-customizer.php',
-        'WP_Skins_Customizer_Abstract' => __DIR__ . '/..' . '/max-kk/wp-skins-customizer/includes/class-wp-skins-customizer-abstract.php',
-        'WP_Skins_Manager_Abstract' => __DIR__ . '/..' . '/max-kk/wp-skins-customizer/includes/class-wp-skins-manager-abstract.php',
-        'underDEV\\Utils\\Settings' => __DIR__ . '/..' . '/underdev/settings/src/Settings.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Checkbox' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Checkbox.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Editor' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Editor.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Number' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Number.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Range' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Range.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Select' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Select.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Text' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Text.php',
-        'underDEV\\Utils\\Settings\\CoreFields\\Url' => __DIR__ . '/..' . '/underdev/settings/src/Settings/CoreFields/Url.php',
-        'underDEV\\Utils\\Settings\\Field' => __DIR__ . '/..' . '/underdev/settings/src/Settings/Field.php',
-        'underDEV\\Utils\\Settings\\Group' => __DIR__ . '/..' . '/underdev/settings/src/Settings/Group.php',
-        'underDEV\\Utils\\Settings\\Section' => __DIR__ . '/..' . '/underdev/settings/src/Settings/Section.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
